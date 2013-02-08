9ad377338346874b466536b58620b2397e48c92c
Mike Mulvaney <mike.mulvaney@gmail.com>
2013-02-08 02:43:22 +0000

    fixed indentation

--- lib/redmine_revision_diff_links/hooks.rb
+++ lib/redmine_revision_diff_links/hooks.rb
@@ -2,6 +2,6 @@ module RedmineRevisionDiffLinks
   
   class Hooks < Redmine::Hook::ViewListener
     render_on :view_layouts_base_html_head,
-    :partial => 'issues/head_include'
+      :partial => 'issues/head_include'
   end
 end
